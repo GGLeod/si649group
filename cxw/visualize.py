aa4adc74b245fc962f212c2613d464a644d76d15
Froogoo <90798340+Froogoo@users.noreply.github.com>
2023-04-21 21:08:20 -0400

    Update visualize.py

--- cxw/visualize.py
+++ cxw/visualize.py
@@ -2,7 +2,7 @@ import pandas as pd
 import altair as alt
 from vega_datasets import data
 
-filter_list = ["Albert","Newfoundland and Labrador", "British Columbia"]
+filter_list = ["Alberta","Newfoundland and Labrador", "British Columbia"]
 df = pd.read_csv("cxw/zRankings.csv")[~pd.read_csv("cxw/zRankings.csv")["State_full"].isin(filter_list)]
 state_map = data.us_10m.url
 states = alt.topo_feature(state_map, feature='states')
